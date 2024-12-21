df1b91d32a45551e2936045185513fbba6bc8395
Andrew <andfrewluferenko@icloud.com>
2024-12-21 16:17:59 +0300

    created exceptions and GlobalExceptionHandler

---
+++ src/main/java/com/backspark/socks/exception/InsufficientSocksException.java
@@ -0,0 +1,7 @@
+package com.backspark.socks.exception;
+
+public class InsufficientSocksException extends RuntimeException {
+    public InsufficientSocksException(String message) {
+        super(message);
+    }
+}
