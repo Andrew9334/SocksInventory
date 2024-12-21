a80abc86773b690ce43b4138c1a1edef03f75c62
Andrew <andfrewluferenko@icloud.com>
2024-12-21 15:42:18 +0300

    created model

---
+++ src/main/java/com/backspark/socks/model/Sock.java
@@ -0,0 +1,49 @@
+package com.backspark.socks.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Sock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String color;
+
+    private Integer cottonPart;
+
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getCottonPart() {
+        return cottonPart;
+    }
+
+    public void setCottonPart(Integer cottonPart) {
+        this.cottonPart = cottonPart;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
